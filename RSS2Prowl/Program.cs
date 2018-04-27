893a04ef6c54797bda4f54c17b96cfbf6a31977d
Philipp Baum <baum@webix.de>
2018-04-27 16:18:08 +0200

    Init

---
+++ RSS2Prowl/Program.cs
@@ -0,0 +1,51 @@
+﻿using Prowlin;
+using Prowlin.Interfaces;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Xml;
+
+namespace RSS2Prowl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string feed = "-";
+            string applicationName = "BGG";
+            string archiveFileName = "archive.txt";
+
+            FeedParser parser = new FeedParser();
+            var items = parser.ReadFeed(feed);
+
+            foreach (var item in items)
+            {
+
+                //Check if notification for this item was sent already
+                if (File.ReadAllText(archiveFileName).Contains(item.Md5Hash))
+                {
+                    continue;
+                }
+
+                //sent notification
+                INotification notification = new Prowlin.Notification()
+                {
+                    Application = applicationName,
+                    Description = "",//item.Content,
+                    Event = item.Title,
+                    Priority = NotificationPriority.Normal,
+                    Url = item.Url
+                };
+                notification.AddApiKey("-");
+
+                ProwlClient prowlClient = new ProwlClient();
+                NotificationResult notificationResult = prowlClient.SendNotification(notification);
+
+                //Add to archive file
+                File.AppendAllText(archiveFileName, item.Md5Hash + Environment.NewLine);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ RSS2Prowl/RssParser.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace RSS2Prowl
+{
+    public class FeedParser
+    {
+        public List<Item> ReadFeed(string url)
+        {
+            try
+            {
+                XDocument doc = XDocument.Load(url);
+                var entries = from item in doc.Root.Descendants().First(i => i.Name.LocalName == "channel").Elements().Where(i => i.Name.LocalName == "item")
+                              select new Item
+                              {
+                                  Content = item.Elements().First(i => i.Name.LocalName == "description").Value,
+                                  Url = item.Elements().First(i => i.Name.LocalName == "link").Value,
+                                  Title = item.Elements().First(i => i.Name.LocalName == "title").Value,
+                                  Md5Hash = CreateMD5(item.Elements().First(i => i.Name.LocalName == "title").Value + item.Elements().First(i => i.Name.LocalName == "pubDate").Value)
+
+                              };
+                return entries.ToList();
+            }
+            catch
+            {
+                return new List<Item>();
+            }
+        }
+
+        //https://stackoverflow.com/questions/11454004/calculate-a-md5-hash-from-a-string
+        private string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+
+    public class Item
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Content { get; set; }
+
+        public string Md5Hash { get; set; }
+
+        public Item()
+        {
+            Title = "";
+            Url = "";
+            Content = "";
+        }
+    }
+
+}
